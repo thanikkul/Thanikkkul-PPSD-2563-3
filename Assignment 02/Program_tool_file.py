f43cfce60b32ddbd3b5d129de7d46ccd213ddbcb
THANIKKUL PROMSREE <81483735+thanikkul@users.noreply.github.com>
2021-05-29 03:56:58 +0700

    Add files via upload

--- Assignment 02/Program_tool_file.py
+++ Assignment 02/Program_tool_file.py
@@ -1,5 +1,5 @@
 # HW_2
-def menu():
+def menu(): #function menu
     k_menu = input(
         'Menu Program\n Enter W write data to file\n Enter R read data from file\n Enter A append data file\n Enter D remove data from file\n Enter You :')
     if k_menu == 'W':
@@ -14,7 +14,7 @@ def menu():
         print('Please Select W , R , A or D')
 
 
-def write_file():
+def write_file(): #function for write File
     f = open('test.txt', 'w')
     data_list = []
     while True:
@@ -29,40 +29,12 @@ def write_file():
     f.close()
 
 
-def read_file():
+def read_file(): #function for read File
     f = open('test.txt')
     data = f.read()
     print(data)
     f.close()
 
-
-def append_file():
-    f = open('test.txt', 'a+')
-    data_list = []
-    while True:
-        try:
-            data_input = int(input('Enter a number :'))
-            data_list.append(str(data_input))
-            select = input(
-                'Enter S stop record file \nEnter N write  data to file again\nEnter a Select :')
-            if select == 'S':
-                break
-            elif select == 'N':
-                pass
-            else:
-                break
-        except:
-            print('Please Enter a number')
-            break
-
-    f.write(" ".join(data_list))
-    f.close()
-
-
-def delete_file():
-    f = open("test.txt", 'w')
-    f.write("")
-    f.close()
 
 
 menu()
